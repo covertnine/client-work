5f6f0f6a69e494216dfc66f86d0c10e10c234003
COVERT NINE <tim@covertnine.com>
2019-08-13 18:53:27 -0500

    header diagonals, footer for client, css for footer widgets and style guide elements

---
+++ inc/footer.php
@@ -0,0 +1,34 @@
+<div class="footer-wrapper" id="wrapper-footer">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-12">
+
+                <footer class="site-footer" id="colophon">
+
+
+                    <div class="site-info">
+
+                        <div class="container">
+                            <div class="row text-center">
+                                <div class="col-xs-12">
+                                    <p class="text-center">&copy; <?php echo date("Y") . ' ';
+                                                                    echo bloginfo('name'); ?> | Owned by Marmon Group/Berkshire Hathaway Company</p>
+                                </div>
+                            </div>
+                        </div><!-- .container-->
+
+                    </div><!-- .site-info -->
+
+                </footer><!-- #colophon -->
+
+            </div>
+            <!--col end -->
+
+        </div><!-- row end -->
+
+    </div><!-- container end -->
+
+</div><!-- wrapper end -->
