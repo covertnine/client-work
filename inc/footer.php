82a7a6d5aae341ce34198101e32fcc59dafcd4b0
COVERT NINE <tim@covertnine.com>
2019-08-13 19:16:28 -0500

    footer styling

--- inc/footer.php
+++ inc/footer.php
@@ -13,9 +13,28 @@
 
                         <div class="container">
                             <div class="row text-center">
-                                <div class="col-xs-12">
-                                    <p class="text-center">&copy; <?php echo date("Y") . ' ';
-                                                                    echo bloginfo('name'); ?> | Owned by Marmon Group/Berkshire Hathaway Company</p>
+                                <div class="col-xs-6 col-sm-3 col-lg-2 p-0">
+                                    <div class="footer-social text-center">
+                                        <a href="https://www.pinterest.com/WellsLamont/" target="_blank"><i class="fab fa-pinterest"></i></a>
+                                        <a href="https://www.facebook.com/WellsLamont" target="_blank"><i class="fab fa-facebook-official"></i></a>
+                                        <a href="https://www.instagram.com/WellsLamont" target="_blank"><i class="fab fa-instagram"></i></a>
+                                        <a href="https://www.linkedin.com/company/wells-lamont-retail" target="_blank"><i class="fab fa-linkedin-square"></i></a>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6 col-sm-3 col-lg-3">
+                                    <div class="footer-links">
+                                        <a href="#">Terms</a> |
+                                        <a href="#">Privacy</a>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-6 col-lg-5 p-0">
+                                    <p class="text-center copyright">&copy; <?php echo date("Y") . ' ';
+                                                                            echo bloginfo('name'); ?> | A Marmon Group/Berkshire Hathaway Company</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-lg-2 text-left">
+                                    <div class="footer-search">
+                                        [search]
+                                    </div>
                                 </div>
                             </div>
                         </div><!-- .container-->
