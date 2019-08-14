bef8aa8b5d657cc2e1c6d59335f485a63e9a190c
COVERT NINE <tim@covertnine.com>
2019-08-13 19:29:31 -0500

    footer widget adjustments on ombile

--- inc/footer.php
+++ inc/footer.php
@@ -13,7 +13,7 @@
 
                         <div class="container">
                             <div class="row text-center">
-                                <div class="col-xs-6 col-sm-3 col-lg-2 p-0">
+                                <div class="col-xs-6 col-sm-3 col-lg-2 p-0 footer-social-wrapper">
                                     <div class="footer-social text-center">
                                         <a href="https://www.pinterest.com/WellsLamont/" target="_blank"><i class="fab fa-pinterest"></i></a>
                                         <a href="https://www.facebook.com/WellsLamont" target="_blank"><i class="fab fa-facebook-official"></i></a>
@@ -21,17 +21,17 @@
                                         <a href="https://www.linkedin.com/company/wells-lamont-retail" target="_blank"><i class="fab fa-linkedin-square"></i></a>
                                     </div>
                                 </div>
-                                <div class="col-xs-6 col-sm-3 col-lg-3">
+                                <div class="col-xs-6 col-sm-3 col-lg-3 footer-links-wrapper">
                                     <div class="footer-links">
                                         <a href="#">Terms</a> |
                                         <a href="#">Privacy</a>
                                     </div>
                                 </div>
-                                <div class="col-xs-12 col-sm-6 col-lg-5 p-0">
+                                <div class="col-xs-12 col-sm-6 col-lg-5 p-0 footer-copyright-wrapper">
                                     <p class="text-center copyright">&copy; <?php echo date("Y") . ' ';
                                                                             echo bloginfo('name'); ?> | A Marmon Group/Berkshire Hathaway Company</p>
                                 </div>
-                                <div class="col-xs-12 col-sm-12 col-lg-2 text-left">
+                                <div class="col-xs-12 col-sm-12 col-lg-2 text-left footer-search-wrapper">
                                     <div class="footer-search">
                                         [search]
                                     </div>
