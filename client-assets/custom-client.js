04f2fe25e19909508e8dd2af9c0d4ddb12aa4450
COVERT NINE <tim@covertnine.com>
2020-01-14 19:29:26 -0600

    cleaning up client functions and removing unused code

--- client-assets/custom-client.js
+++ client-assets/custom-client.js
@@ -1,40 +1,11 @@
 jQuery(document).ready(function() {
   (function($) {
-    console.log("sick");
     ///////////////////////// Move the content up by the height of the navbar object for a transparent nav effect ////
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     if ($(window).width() <= 667) {
       $("#page-wrapper").css("margin-top", "0");
     }
 
-    //collapse the mega menu sub navs in the products menu for small screens
-    if ($(window).width() <= 568) {
-      $(
-        ".mega-sub-menu .mega-sub-menu .mega-sub-menu > .mega-menu-item:not('.widget_custom_html')"
-      ).addClass("hide");
-      $(
-        ".mega-sub-menu .mega-sub-menu .mega-sub-menu > .widget_custom_html > .mega-block-title"
-      ).on("click", function() {
-        $(this)
-          .parent()
-          .nextAll()
-          .toggleClass("hide");
-      });
-    }
-
-    //bouncing arrow
-    $(window).scroll(function() {
-      //scroll position variable
-      var scroll = $(window).scrollTop();
-
-      if (scroll >= 68) {
-        $(".bouncing-arrow").fadeOut();
-      }
-      if (scroll <= 67) {
-        $(".bouncing-arrow").fadeIn();
-      }
-    }); //end bouncing arrow
-
-    $('.woof_redraw_zone').css({"opacity": "1"});
+    $(".woof_redraw_zone").css({ opacity: "1" });
   })(jQuery); //end jquery function
 }); //end document ready
--- inc/client-functions.php
+++ inc/client-functions.php
@@ -1,68 +1,126 @@
 <?php
-//functions for client specifics Wells Lamont
-
-/****************************************************************************************/
-/***************************** remove admin bar
-/****************************************************************************************/
-add_action('after_setup_theme', 'remove_admin_bar');
-function remove_admin_bar()
-{
-    if (!current_user_can('administrator') && !is_admin()) {
-        show_admin_bar(false);
-    }
-}
-
-/****************************************************************************************/
-/***************************** client color settings
-/****************************************************************************************/
-include("client-setup.php");
+//functions for client specifics
 
 /****************************************************************************************/
 /***************************** load client scripts for frontend styling
 /****************************************************************************************/
+/**
+ * Client-specific functionality
+ *
+ * @package c9
+ */
 
-if (!function_exists('client_scripts')) {
-    /**
-     * Load theme's JavaScript and CSS sources.
-     */
-    function client_scripts()
-    {
-        //wp_enqueue_style('client-type', 'https://use.typekit.net/uqa4rne.css', array('cortextoo-styles'));
-        wp_enqueue_style('cortextoo-megamenu', get_stylesheet_directory_uri() . '/client/client-assets/vendor/megamenu.css', array('c9-styles'));
-        wp_enqueue_style('client-styles', get_stylesheet_directory_uri() . '/client/client-assets/dist/client.min.css', array('c9-blocks-style'));
-        wp_enqueue_style('client-styles', get_stylesheet_directory_uri() . '/client/client-assets/dist/client-editor.min.css', 99999999);
-        wp_enqueue_script('client-scripts', get_template_directory_uri() . '/client/client-assets/custom-client.js', array('jquery'), true);
-    }
+if ( ! function_exists( 'client_scripts' ) ) {
+	/**
+	 * Load theme's JavaScript and CSS sources.
+	 */
+	function client_scripts() {
+
+		wp_enqueue_style( 'client-styles', get_template_directory_uri() . '/client/client-assets/dist/client.min.css', array( 'c9-styles' ) );
+		wp_enqueue_script( 'client-scripts', get_template_directory_uri() . '/client/client-assets/custom-client.js', array( 'jquery', 'smooth-state' ), true );
+
+		//some examples of extending scripts
+		//wp_enqueue_script( 'smooth-state', get_template_directory_uri() . '/client/client-assets/vendor/jquery.smoothState.min.js', array( 'jquery' ), true );
+		//wp_enqueue_style( 'c9-megamenu', get_template_directory_uri() . '/client/client-assets/vendor/megamenu.css', array( 'c9-styles' ) );
+
+	}
 } // endif function_exists( 'client_scripts' ).
-add_action('wp_enqueue_scripts', 'client_scripts', 2);
+add_action( 'wp_enqueue_scripts', 'client_scripts', 20 );
 
-/* add client compiled files to gutenberg editor */
-if (!function_exists('c9_client_editor_style')) {
-    function c9_client_editor_style()
-    {
-        wp_enqueue_style('c9-client-typekit-style', '//use.typekit.net/uqa4rne.css');
-        wp_enqueue_style('c9-client-styles', get_stylesheet_directory_uri() . '/client/client-assets/dist/client.css');
-        wp_enqueue_style('c9-client-editor-styles', get_stylesheet_directory_uri() . '/client/client-assets/dist/client-editor.css');
-    }
-    add_action('enqueue_block_editor_assets', 'c9_client_editor_style', 99999999);
+if ( ! function_exists( 'c9_client_editor_style' ) ) {
+	/**
+	 * Add client compiled files to gutenberg editor.
+	 */
+	function c9_client_editor_style() {
+		wp_enqueue_style( 'c9-client-styles', get_template_directory_uri() . '/client/client-assets/dist/client.min.css' );
+		wp_enqueue_style( 'c9-client-editor-styles', get_template_directory_uri() . '/client/client-assets/dist/client-editor.min.css' );
+	}
+	add_action( 'enqueue_block_editor_assets', 'c9_client_editor_style', 99999999 );
 } //end if function exists
 
-/****************************************************************************************/
-/***************************** custom fields
-/****************************************************************************************/
-define('ACF_LITE', true);
-
-/****************************************************************************************/
-/***************************** hide non-essential admin
-/****************************************************************************************/
-function remove_admin_menu_items()
-{
-    remove_menu_page('pmxi-admin-home');
-    remove_menu_page('wr2x_settings-menu');
-    remove_menu_page('meowapps-main-menu');
+add_action( 'after_setup_theme', 'c9_client_setup' );
+if ( ! function_exists( 'c9_client_setup' ) ) {
+	/**
+	 * Enable support for Post Formats.
+	 * See http://codex.wordpress.org/Post_Formats
+	 */
+	function c9_client_setup() {
+		add_theme_support(
+			'post-formats',
+			array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+			)
+		);
+	}
 }
-add_action('admin_menu', 'remove_admin_menu_items', 999);
 
+add_action( 'after_setup_theme', 'c9_add_woocommerce_support' );
+if ( ! function_exists( 'c9_add_woocommerce_support')) {
+	/**
+	 * Enable support for WooCommerce
+	 */
+	function c9_add_woocommerce_support() {
+        add_theme_support( 'woocommerce' );
+        add_theme_support('wc-product-gallery-zoom');
+        add_theme_support('wc-product-gallery-lightbox');
+        add_theme_support('wc-product-gallery-slider');
+	}
+}
+
+// $path defaults to 'woocommerce/' (in client folder)
+add_filter('woocommerce_template_path', function () {
+    return 'client/woocommerce/';
+});
+
+
+// change woocommerce thumbnail image size
+add_filter('woocommerce_get_image_size_gallery_thumbnail', 'c9_override_woocommerce_image_size_gallery_thumbnail');
+function c9_override_woocommerce_image_size_gallery_thumbnail($size)
+{
+    // Gallery thumbnails: proportional, max width 200px
+    return array(
+        'width'  => 85,
+        'height' => 85,
+        'crop'   => 0,
+    );
+}
+
+
+
+add_filter('woocommerce_gallery_thumbnail_size', function ($size) {
+    return 'woocommerce_thumbnail';
+});
+
+
+
+
+/** * Change number of products that are displayed per page (shop page) */
+add_filter('loop_shop_per_page', 'c9_loop_shop_per_page', 40);
+function c9_loop_shop_per_page($cols) {
+
+    // $cols contains the current number of products per page based on the value stored on Options -> Reading 
+    // Return the number of products you wanna show per page. 
+    $cols = 40;
+    return $cols;
+}
+
+/****************************************************************************************/
+/******** Adding filter to look for client folder templates before child theme templates
+/****************************************************************************************/
+add_filter( 'template_include', function( $template ) {
+  $path = explode('/', $template );
+  $template_chosen = end( $path );
+  $grandchild_template = get_template_directory() . '/client/' . $template_chosen;
+  if ( file_exists( $grandchild_template  ) ) {
+     	$template = $grandchild_template;
+  }
+  return $template;
+});
+/****************************************************************************************/
 
 /****************************************************************************************/
 /***************************** nav search form
@@ -74,124 +132,3 @@ function cortextoo_add_search_form($items, $args)
         $items .= '<li class="search"><form role="search" method="get" id="searchform" action="' . home_url('/') . '"><label class="sr-only">Search Site</label><input type="text" value="" placeholder="Search" name="s" id="s" /><input type="submit" id="searchsubmit" value="' . esc_attr__('Search') . '" class="hide"/></form></li>';
     return $items;
 }
-
-/****************************************************************************************/
-/***************************** WooCommerce Specifics
-/****************************************************************************************/
-//declare support
-function c9_add_woocommerce_support()
-{
-    add_theme_support('woocommerce');
-    // add_theme_support('wc-product-gallery-zoom');
-    add_theme_support('wc-product-gallery-lightbox');
-    add_theme_support('wc-product-gallery-slider');
-}
-add_action('after_setup_theme', 'c9_add_woocommerce_support');
-
-//remove related items
-remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
-
-// $path defaults to 'woocommerce/' (in client folder)
-add_filter('woocommerce_template_path', function () {
-    return 'client/woocommerce/';
-});
-
-// change woocommerce thumbnail image size
-add_filter('woocommerce_get_image_size_gallery_thumbnail', 'override_woocommerce_image_size_gallery_thumbnail');
-function override_woocommerce_image_size_gallery_thumbnail($size)
-{
-    // Gallery thumbnails: proportional, max width 200px
-    return array(
-        'width'  => 85,
-        'height' => 85,
-        'crop'   => 0,
-    );
-}
-
-
-add_filter('woocommerce_gallery_thumbnail_size', function ($size) {
-    return 'woocommerce_thumbnail';
-});
-
-/** * Change number of products that are displayed per page (shop page) */
-add_filter('loop_shop_per_page', 'new_loop_shop_per_page', 40);
-function new_loop_shop_per_page($cols)
-{
-
-    // $cols contains the current number of products per page based on the value stored on Options -> Reading 
-    // Return the number of products you wanna show per page. 
-    $cols = 40;
-    return $cols;
-}
-
-add_filter('woocommerce_product_tabs', 'c9_theme_product_tabs');
-
-//add product specs tab and field
-function c9_theme_product_tabs($tabs)
-{
-
-    // ensure ACF is available
-    if (!function_exists('get_field'))
-        return;
-
-    $content = trim(get_field('glove_specs'));
-
-    if (!empty($content)) {
-
-        $tabs['desc_tab'] = array(
-            'title' => 'Specs',
-            'priority' => 15,
-            'callback' => 'c9_theme_prod_specs'
-        );
-    }
-
-    return $tabs;
-}
-
-function c9_theme_prod_specs()
-{
-    $prod_id = get_the_ID();
-    echo get_post_meta($prod_id, 'glove_specs', true);
-}
-
-add_filter( 'woocommerce_helper_suppress_admin_notices', '__return_true' );
-
-/* changing # of items in pagination */
-function wl_posts_per_page( $query ) {
-	if ( ! $query->is_main_query() || is_admin() ) {
-return;
-	}
-
-	$query->set( 'posts_per_page', 24 );
-	return;
-}
-add_action( 'pre_get_posts', 'wl_posts_per_page', 99999 );
-
-/**
- * Rename product data tabs
- */
-add_filter( 'woocommerce_product_tabs', 'woo_rename_tabs', 98 );
-function woo_rename_tabs( $tabs ) {
-
-	$tabs['description']['title'] = __( 'Details' );		// Rename the description tab
-	$tabs['additional_information']['title'] = __( 'Specs' );	// Rename the additional information tab
-
-	return $tabs;
-
-}
-
-/****************************************************************************************/
-/******** Adding filter to look for client folder templates before child theme templates
-/****************************************************************************************/
-
-add_filter( 'template_include', function( $template ) {
-  $path = explode('/', $template );
-  $template_chosen = end( $path );
-  $grandchild_template = get_template_directory() . '/client/' . $template_chosen;
-  if ( file_exists( $grandchild_template  ) ) {
-     	$template = $grandchild_template;
-  }
-
-  return $template;
-
-}, 99);
