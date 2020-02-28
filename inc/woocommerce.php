ba8f9653bf3842e0ddf1376a1a32e85e4cec7a8d
COVERT NINE <tim@covertnine.com>
2020-02-28 00:40:01 -0600

    refatoring code

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -17,6 +17,49 @@ if ( ! function_exists( 'c9_add_woocommerce_support')) {
 	}
 }
 
+// /**
+//  * Register our sidebars and widgetized areas.
+//  */
+// function woocommerce_product_filter()
+// {
+//     register_sidebar(
+//         array(
+//         'name'          => 'Product Filter Header',
+//         'id'            => 'wc_product_filter',
+//         'before_widget' => '<div>',
+//         'after_widget'  => '</div>',
+//         'before_title'  => '<h2 class="filter">',
+//         'after_title'   => '</h2>',
+//         )
+//     );
+// }
+// add_action('widgets_init', 'woocommerce_product_filter');
+
+
+/* Customize Product Features Labels */
+
+add_filter('woocommerce_taxonomy_args_product_tag', 'custom_wc_taxonomy_label_product_tag');
+function custom_wc_taxonomy_label_product_tag( $args )
+{
+    $args['label'] = 'Product Feature';
+    $args['labels'] = array(
+        'name'                 => __('Product Features', 'woocommerce'),
+        'singular_name'     => __('Product Feature', 'woocommerce'),
+        'menu_name'            => _x('Features', 'Admin menu name', 'woocommerce'),
+        'search_items'         => __('Search Product Features', 'woocommerce'),
+        'all_items'         => __('All Product Features', 'woocommerce'),
+        'parent_item'         => __('Parent Product Feature', 'woocommerce'),
+        'parent_item_colon' => __('Parent Product Feature:', 'woocommerce'),
+        'edit_item'         => __('Edit Product Feature', 'woocommerce'),
+        'update_item'         => __('Update Product Feature', 'woocommerce'),
+        'add_new_item'         => __('Add New Product Feature', 'woocommerce'),
+        'new_item_name'     => __('New Product Feature Name', 'woocommerce')
+    );
+
+    return $args;
+}
+
+
 // $path defaults to 'woocommerce/' (in client folder)
 add_filter('woocommerce_template_path', function () {
     return 'client/woocommerce/';
