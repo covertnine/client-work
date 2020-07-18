8c8b5a98d0916a5b47128ce5bb455505f35a884c
tim <tim@covertnine.com>
2020-07-18 15:11:53 -0500

    adding sample content

--- inc/client-setup.php
+++ inc/client-setup.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Color scheme, style variations for core blocks, and post formats setup
+ * Color scheme, style variations for core blocks, sample content, and post formats setup
  *
  * @package c9
  */
@@ -56,8 +56,67 @@ register_block_style(
 );
 
 if (!function_exists('c9_client_setup')) {
-    function c9_client_setup()
-    {
+    function c9_client_setup() {
+
+		add_theme_support('starter-content',
+			array(
+				'options'	=> array(
+					'show_on_front'	=> 'page',
+					'page_on_front'	=> '{{home}}',
+					'page_for_posts' => '{{blog}}',
+					'blogname'		=> 'C9 Work',
+					'blogdescription'	=> 'A WordPress Theme for industrial businesses with product catalog and mega menu support.'
+				),
+				'theme_mods'	=> array(
+					'c9_show_search'		=> 'show',
+					'c9_copyright_content'	=> '&copy; 2020. <a href="https://www.covertnine.com" title="Web design company in Chicago" target="_blank">WordPress Website design by COVERT NINE</a>.',
+					'c9_default_font'		=> 'yes',
+					'c9_heading_font'		=> 'Bebas Neue',
+					'c9_subheading_font'	=> 'Lobster',
+					'c9_body_font'			=> '',
+					'c9_author_visible'		=> 'hide',
+					'c9_blog_sidebar'		=> 'hide',
+					'c9_archive_sidebar'	=> 'hide'
+
+				),
+				'nav_menus'	=> array(
+					'primary'	=> array(
+						'name'	=>	__( 'Top Navigation Menu', 'c9-work' ),
+						'items'	=> array(
+							'link_home',
+							'page_about',
+							'page_contact',
+							'page_blog',
+						),
+					),
+				),
+				'widgets'	=> array(
+					'footerfull'	=> array(
+						'text_about',
+						'recent-posts',
+						'recent-comments',
+						'meta_custom' => array( 'meta', array(
+							'title'	=> 'Meta Widget',
+						) ),
+					),
+					'right-sidebar'	=> array(
+						'search',
+						'text_about',
+						'meta_custom' => array( 'meta', array(
+							'title'	=> 'Meta Widget',
+						) ),
+					),
+					'left-sidebar'	=> array(
+						'search',
+						'text_about',
+						'meta_custom' => array( 'meta', array(
+							'title'	=> 'Meta Widget',
+						) ),
+					),
+				),
+			)
+		);
+
 		add_theme_support(
 			'post-formats',
 			array(
