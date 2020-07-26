f146a51e96d7c1be8475a95838963191fee640f7
tim <tim@covertnine.com>
2020-07-26 02:07:52 -0500

    removing unsplash and fixhing get contents functions

--- inc/client-setup.php
+++ inc/client-setup.php
@@ -1,66 +1,69 @@
 <?php
+
 /**
  * Color scheme, style variations for core blocks, sample content, and post formats setup
  *
  * @package c9
  */
 
- // Registers a style variation for separators
+// Registers a style variation for separators
 register_block_style(
-    'core/separator',
-    array(
-        'name'         => 'black-bar',
-        'label'        => __( 'Full Width Black Bar', 'c9-work' ),
-    )
+	'core/separator',
+	array(
+		'name'         => 'black-bar',
+		'label'        => __('Full Width Black Bar', 'c9-work'),
+	)
 );
 
 register_block_style(
-    'c9-blocks/grid',
-    array(
-        'name'         => 'gray-diagonal',
-        'label'        => __( 'Gray Diagonal Background', 'c9-work' ),
+	'c9-blocks/grid',
+	array(
+		'name'         => 'gray-diagonal',
+		'label'        => __('Gray Diagonal Background', 'c9-work'),
 	)
 
 );
 
 register_block_style(
-    'c9-blocks/grid',
+	'c9-blocks/grid',
 	array(
-			'name'		  => 'dark-gradient-left',
-			'label'		  => __( 'Left Side Dark Gradient Background', 'c9-work' ),
+		'name'		  => 'dark-gradient-left',
+		'label'		  => __('Left Side Dark Gradient Background', 'c9-work'),
 	)
 );
 
 register_block_style(
-    'core/image',
+	'core/image',
 	array(
-			'name'		  => 'img-shadow',
-			'label'		  => __( 'Shadow Below', 'c9-work' ),
+		'name'		  => 'img-shadow',
+		'label'		  => __('Shadow Below', 'c9-work'),
 	)
 );
 
 register_block_style(
-    'c9-blocks/heading',
+	'c9-blocks/heading',
 	array(
-			'name'		  => 'abel-heading',
-			'label'		  => __( 'Second Heading', 'c9-work' ),
+		'name'		  => 'abel-heading',
+		'label'		  => __('Second Heading', 'c9-work'),
 	)
 );
 
 register_block_style(
-    'core/paragraph',
+	'core/paragraph',
 	array(
-			'name'		  => 'black-bar',
-			'label'		  => __( 'Black Bar Behind', 'c9-work' ),
+		'name'		  => 'black-bar',
+		'label'		  => __('Black Bar Behind', 'c9-work'),
 	)
 );
 
 if (!function_exists('c9_client_setup')) {
-    function c9_client_setup() {
+	function c9_client_setup()
+	{
 
 		global $wp_filesystem;
 
-		add_theme_support('starter-content',
+		add_theme_support(
+			'starter-content',
 			array(
 				'attachments'	=> array(
 					'c9work-home-img'	=> array(
@@ -87,34 +90,34 @@ if (!function_exists('c9_client_setup')) {
 				'posts'	=> array(
 					'home'			=> array(
 						'comment_status'	=> 'closed',
-						'post_content'		=> $wp_filesystem->get_contents( get_template_directory_uri() . '/client/content/home.html' )
+						'post_content'		=>  file_get_contents(get_template_directory_uri() . '/client/content/home.html')
 						// 'thumbnail'			=> '{{c9work-home-img}}',
 					),
 					'about'			=> array(
 						// 'thumbnail'			=> '{{c9work-about-img}}',
 						'comment_status'	=> 'closed',
 						'post_type'			=> 'page',
-						'post_title'		=> __( 'Our History', 'c9-work' ),
-						'post_content'		=> $wp_filesystem->get_contents( get_template_directory_uri() . '/client/content/about.html' )
+						'post_title'		=> __('Our History', 'c9-work'),
+						'post_content'		=>  file_get_contents(get_template_directory_uri() . '/client/content/about.html')
 					),
 					'services'		=> array(
 						'comment_status'	=> 'closed',
 						'post_type'			=> 'page',
-						'post_title'		=> __( 'Services', 'c9-work' ),
-						'post_content'		=> $wp_filesystem->get_contents( get_template_directory_uri() . '/client/content/services.html' )
+						'post_title'		=> __('Services', 'c9-work'),
+						'post_content'		=>  file_get_contents(get_template_directory_uri() . '/client/content/services.html')
 					),
 					'styleguide'		=> array(
 						'comment_status'	=> 'closed',
 						'post_type'			=> 'page',
-						'post_title'		=> __( 'Style Guide', 'c9-work' ),
-						'post_content'		=> $wp_filesystem->get_contents( get_template_directory_uri() . '/client/content/styleguide.html' )
+						'post_title'		=> __('Style Guide', 'c9-work'),
+						'post_content'		=>  file_get_contents(get_template_directory_uri() . '/client/content/styleguide.html')
 					),
 					'contact'		=> array(
 						'comment_status'	=> 'closed',
-						'post_content'		=> $wp_filesystem->get_contents( get_template_directory_uri() . '/client/content/contact.html' )
+						'post_content'		=>  file_get_contents(get_template_directory_uri() . '/client/content/contact.html')
 					),
 					'news'			=> array(
-						'post_content'			=> __( 'This page will show all of the news posts once you have populated your database with news items.', 'c9-work'),
+						'post_content'			=> __('This page will show all of the news posts once you have populated your database with news items.', 'c9-work'),
 						'thumbnail'			=> '{{c9work-news-img}}',
 					),
 				),
@@ -139,26 +142,26 @@ if (!function_exists('c9_client_setup')) {
 				),
 				'nav_menus'		=> array(
 					'primary'		=> array(
-						'name'			=>	__( 'Top Navigation Menu', 'c9-work' ),
+						'name'			=>	__('Top Navigation Menu', 'c9-work'),
 						'items'			=> array(
-									'page_home',
-									'page_about'	=> array(
-										'type'		=> 'post_type',
-										'object'	=> 'page',
-										'object_id'	=> '{{about}}',
-									),
-									'page_services'	=> array(
-										'type'		=> 'post_type',
-										'object'	=> 'page',
-										'object_id'	=> '{{services}}',
-									),
-									'page_styleguide'	=> array(
-										'type'		=> 'post_type',
-										'object'	=> 'page',
-										'object_id'	=> '{{styleguide}}',
-									),
-									'page_news',
-									'page_contact',
+							'page_home',
+							'page_about'	=> array(
+								'type'		=> 'post_type',
+								'object'	=> 'page',
+								'object_id'	=> '{{about}}',
+							),
+							'page_services'	=> array(
+								'type'		=> 'post_type',
+								'object'	=> 'page',
+								'object_id'	=> '{{services}}',
+							),
+							'page_styleguide'	=> array(
+								'type'		=> 'post_type',
+								'object'	=> 'page',
+								'object_id'	=> '{{styleguide}}',
+							),
+							'page_news',
+							'page_contact',
 						),
 					),
 				),
@@ -190,7 +193,7 @@ if (!function_exists('c9_client_setup')) {
 						'c9work_company'	=> array(
 							'text',
 							array(
-								'title'	=> __( 'Company Menu', 'c9-work' ),
+								'title'	=> __('Company Menu', 'c9-work'),
 								'text'	=> '<ul id="menu-footer-company" class="menu">
 									<li class="menu-item">
 										<a href="#">Catalog</a>
@@ -210,13 +213,13 @@ if (!function_exists('c9_client_setup')) {
 								</ul>'
 							)
 						),
-						'meta_custom' => array( 'meta', array(
-							'title'	=> __('WordPress Meta', 'c9-work' ),
-						) ),
+						'meta_custom' => array('meta', array(
+							'title'	=> __('WordPress Meta', 'c9-work'),
+						)),
 						'c9work_about' => array(
 							'text',
 							array(
-								'title'	=> __( 'About C9 Work Theme', 'c9-work' ),
+								'title'	=> __('About C9 Work Theme', 'c9-work'),
 								'text'	=> __('Be sure to activate the <strong>C9 Blocks Plugin</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.', 'c9-work'),
 							)
 						)
@@ -230,9 +233,9 @@ if (!function_exists('c9_client_setup')) {
 								'text'	=> 'Be sure to activate the <strong>C9 Blocks Plugin</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.'
 							)
 						),
-						'meta_custom' => array( 'meta', array(
+						'meta_custom' => array('meta', array(
 							'title'	=> 'Meta Widget',
-						) ),
+						)),
 					),
 					'left-sidebar'	=> array(
 						'search',
@@ -243,9 +246,9 @@ if (!function_exists('c9_client_setup')) {
 								'text'	=> 'Be sure to activate the <strong>C9 Blocks Plugin</strong> during theme setup. The blocks plugin includes the custom WordPress blocks for tabs, toggles, and the responsive grid system that makes the theme look super duper.'
 							)
 						),
-						'meta_custom' => array( 'meta', array(
+						'meta_custom' => array('meta', array(
 							'title'	=> 'Meta Widget',
-						) ),
+						)),
 					),
 				),
 			)
@@ -262,35 +265,35 @@ if (!function_exists('c9_client_setup')) {
 			)
 		);
 
-        // Make specific theme colors available in the editor.
-        add_theme_support('editor-color-palette', array(
-            array(
-                'name' => 'orange',
-                'color'    => '#E8983A',
-                'slug' => 'color-orange',
-            ),
-            array(
-                'name' => 'yellow',
-                'color' => '#FEC50A',
-                'slug'    => 'color-yellow',
-            ),
-            array(
-                'name' => 'gray',
-                'color'    => '#ADB1B2',
-                'slug'    => 'color-gray',
-            ),
-            array(
-                'name' => 'white',
-                'color'    => '#ffffff',
-                'slug'    => 'color-white',
-            ),
-            array(
-                'name' => 'black',
-                'color'    => '#000000',
-                'slug'    => 'color-black',
-            ),
-        ));
-    }
+		// Make specific theme colors available in the editor.
+		add_theme_support('editor-color-palette', array(
+			array(
+				'name' => 'orange',
+				'color'    => '#E8983A',
+				'slug' => 'color-orange',
+			),
+			array(
+				'name' => 'yellow',
+				'color' => '#FEC50A',
+				'slug'    => 'color-yellow',
+			),
+			array(
+				'name' => 'gray',
+				'color'    => '#ADB1B2',
+				'slug'    => 'color-gray',
+			),
+			array(
+				'name' => 'white',
+				'color'    => '#ffffff',
+				'slug'    => 'color-white',
+			),
+			array(
+				'name' => 'black',
+				'color'    => '#000000',
+				'slug'    => 'color-black',
+			),
+		));
+	}
 }
 
 add_action('after_setup_theme', 'c9_client_setup');
