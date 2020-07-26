9e9280153eafd69859600d8c75107c8df6c119e6
tim <tim@covertnine.com>
2020-07-26 09:29:58 -0500

    adding dark logo setting that was accidentally deleted

---
+++ inc/admin-settings.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ * Add fields and configure admin settings API.
+ *
+ * @since   1.0
+ * @package c9
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+if ( ! function_exists( 'c9_work_customize_register' ) ) {
+	/**
+	 * Register basic customizer support.
+	 *
+	 * @param object $wp_customize Customizer reference.
+	 */
+	function c9_work_customize_register( $wp_customize ) {
+
+		$wp_customize->add_setting(
+			'c9_dark_logo',
+			array(
+				'default'           => '',
+				'sanitize_callback' => 'wp_filter_post_kses',
+				'type' 				=> 'theme_mod',
+				'capability' 		=> 'edit_theme_options',
+			)
+		);
+
+		$wp_customize->add_control(
+			new WP_Customize_Image_Control(
+				$wp_customize,
+				'c9_dark_logo',
+				array(
+					'label'      => __( 'Dark Logo', 'c9-work' ),
+					'section'    => 'title_tagline',
+					'settings'   => 'c9_dark_logo'
+				)
+			)
+		);
+
+	}
+}
+add_action( 'customize_register', 'c9_work_customize_register' );
