87f9aff2c4e05f939f50925d31c98107f37df5a5
tim <tim@covertnine.com>
2020-08-21 16:46:27 -0400

    making translatable, escaping

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme.
  *
@@ -9,49 +10,37 @@
 ?>
 <div id="wrapper-navbar" class="header-navbar" itemscope itemtype="http://schema.org/WebSite">
 
-    <a class="skip-link screen-reader-text sr-only" href="#content"><?php esc_html_e( 'Skip to content', 'c9-work' ); ?></a>
+	<nav class="navbar navbar-expand-lg">
 
-    <nav class="navbar navbar-expand-lg">
+		<div class="container-fluid">
+			<?php
 
-        <div class="container-fluid">
-            <?php
+			if (has_custom_logo()) {
+				the_custom_logo();
+			}
+			?>
 
-            // get custom logo, if not set, use customizer logo, if that's not set, show text of site title
-            $c9Logo = get_option('cortex_branding', '');
-            $c9SiteName = get_bloginfo('name');
-
-            if (!empty($c9Logo['logo'])) {
-                ?>
-            <a href="<?php echo get_home_url(); ?>" title="<?php echo $c9SiteName . __(' Homepage', 'c9-work'); ?>" class="navbar-brand custom-logo-link c9-custom-logo">
-                <img src="<?php echo $c9Logo['logo']; ?>" class="c9-home-logo img-fluid c9-custom-logo" alt="<?php echo $c9SiteName . __(' Logo', 'c9-work'); ?>" />
-            </a>
-                <?php
-            } else {
-                the_custom_logo();
-            }
-            ?>
-
-            <div class="navbar-small-buttons">
-                <div class="nav-search">
-                    <a href="#" class="btn-nav-search">
-                        <i class="fa fa-search"></i>
-                        <span class="sr-only"><?php __('Search', 'c9-work'); ?></span>
-                    </a>
+			<div class="navbar-small-buttons">
+				<div class="nav-search">
+					<a href="#" class="btn-nav-search">
+						<i class="fa fa-search"></i>
+						<span class="sr-only"><?php __('Search', 'c9-work'); ?></span>
+					</a>
 				</div>
 				<?php
-if ( !function_exists('max_mega_menu_is_enabled') ) {
+				if (!function_exists('max_mega_menu_is_enabled')) {
 				?>
-                <div class="nav-toggle">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fa fa-bars"></i>
-                    </button>
-				</div>
-<?php } ?>
-            </div><!-- .navbar-small-buttons-->
+					<div class="nav-toggle">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+							<i class="fa fa-bars"></i>
+						</button>
+					</div>
+				<?php } ?>
+			</div><!-- .navbar-small-buttons-->
 
-            <!-- The WordPress Menu goes here -->
+			<!-- The WordPress Menu goes here -->
 			<?php
-			if ( function_exists('max_mega_menu_is_enabled') && max_mega_menu_is_enabled('primary') ) {
+			if (function_exists('max_mega_menu_is_enabled') && max_mega_menu_is_enabled('primary')) {
 				wp_nav_menu(
 					array(
 						'theme_location' => 'primary',
@@ -78,7 +67,7 @@ if ( !function_exists('max_mega_menu_is_enabled') ) {
 			} ?>
 
 
-        </div><!-- .container -->
+		</div><!-- .container -->
 
-    </nav><!-- .site-navigation -->
+	</nav><!-- .site-navigation -->
 </div><!-- .header-navbar-->
