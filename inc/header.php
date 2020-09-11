a80ac6fcb9016d49a6bafa2531dcbda47efa6ee6
tim <tim@covertnine.com>
2020-09-11 01:37:28 -0500

    updating sample content

--- inc/header.php
+++ inc/header.php
@@ -36,8 +36,8 @@
 			// check for dark logo option if it's there show it
 			if ( !empty($c9work_dark_logo) ) {
 				?>
-				<a href="<?php esc_url( get_bloginfo('url') ); ?>" title="<?php esc_attr_e( get_bloginfo('name') ); ?>" rel="home">
-				<img src="<?php echo $c9work_dark_logo; ?>" alt="<?php esc_attr_e( get_bloginfo('name') ); ?>" class="navbar-brand c9-custom-logo c9work-dark-logo" />
+				<a href="<?php echo esc_url( home_url() ); ?>" title="<?php esc_attr( get_bloginfo('name') ); ?>" rel="home">
+				<img src="<?php echo $c9work_dark_logo; ?>" alt="<?php esc_attr( get_bloginfo('name') ); ?>" class="navbar-brand c9-custom-logo c9work-dark-logo" />
 			</a>
 			<?php
 			}
